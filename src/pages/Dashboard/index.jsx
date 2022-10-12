0e45436de6d566928947d48a37fb36f3740fc752
hlruffo <hlruffo@gmail.com>
2022-10-12 17:35:20 -0300

    fetch: changing localhost to vercel's link @ dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -15,101 +15,96 @@ export const Dashboard = () => {
     const [{ value: user, loading, error }, fetchHunches] = useAsyncFn(async () => {
         const res = await axios({
             method: 'get',
-            baseURL: ''https://natrave-web-two-livid.vercel.app'
-                '
-'
-                : 3000',
-url: `/${auth.user.username}`,
+            baseURL: 'https://natrave-web-two-livid.vercel.app',
+
+            url: `/${auth.user.username}`,
 
         })
-    const hunches = res.data.hunches.reduce((acc, hunch) => {
-        acc[hunch.gameId] = hunch
-        return acc
-    }, {})
+        const hunches = res.data.hunches.reduce((acc, hunch) => {
+            acc[hunch.gameId] = hunch
+            return acc
+        }, {})
 
-    return {
-        ...res.data,
-        hunches
-    }
-})
-
-const [games, fetchGames] = useAsyncFn(async (params) => {
-    const res = await axios({
-        method: 'get',
-        baseURL: ''https://natrave-web-two-livid.vercel.app'
-            '
-'
-            : 3000',
-url: '/games',
-        params
+        return {
+            ...res.data,
+            hunches
+        }
     })
-    return res.data
-})
+
+    const [games, fetchGames] = useAsyncFn(async (params) => {
+        const res = await axios({
+            method: 'get',
+            baseURL: 'https://natrave-web-two-livid.vercel.app',
+            url: '/games',
+            params
+        })
+        return res.data
+    })
 
 
-const isLoading = games.loading || loading
-const hasError = games.error || error
-const isDone = !isLoading && !hasError
+    const isLoading = games.loading || loading
+    const hasError = games.error || error
+    const isDone = !isLoading && !hasError
 
 
-useEffect(() => {
-    fetchHunches()
-}, [])
+    useEffect(() => {
+        fetchHunches()
+    }, [])
 
 
-useEffect(() => {
-    fetchGames({ gameTime: currentDate })
+    useEffect(() => {
+        fetchGames({ gameTime: currentDate })
 
-}, [currentDate])
+    }, [currentDate])
 
-if (!auth?.user?.id) {
-    return <Navigate to="/" replace={true} />
+    if (!auth?.user?.id) {
+        return <Navigate to="/" replace={true} />
 
-}
-return (
-    <Fragment>
-        <header className="p-4 bg-red-500 text-white">
-            <div className="container max-w-3xl  flex justify-between">
-                <img src="/imgs/logo-fundo-vermelho.svg" className="w-28 md:w-40" />
-                <a href={`/${auth?.user?.username}`}><Icon name="profile" className="w-10" /></a>
-            </div>
-        </header>
-        <main className='space-y-6'>
-            <section id="header" className=" bg-red-500 text-white">
-                <div className="container max-w-3xl space-y-2 p-4 ">
-                    <span>Olá {auth.user.name}</span>
-                    <h3 className='text-2xl font-bold'>Qual é o seu palpite?</h3>
+    }
+    return (
+        <Fragment>
+            <header className="p-4 bg-red-500 text-white">
+                <div className="container max-w-3xl  flex justify-between">
+                    <img src="/imgs/logo-fundo-vermelho.svg" className="w-28 md:w-40" />
+                    <a href={`/${auth?.user?.username}`}><Icon name="profile" className="w-10" /></a>
                 </div>
-            </section>
+            </header>
+            <main className='space-y-6'>
+                <section id="header" className=" bg-red-500 text-white">
+                    <div className="container max-w-3xl space-y-2 p-4 ">
+                        <span>Olá {auth.user.name}</span>
+                        <h3 className='text-2xl font-bold'>Qual é o seu palpite?</h3>
+                    </div>
+                </section>
 
-            <section id="content" className=' container max-w-3xl p-4 space-y-4 '>
+                <section id="content" className=' container max-w-3xl p-4 space-y-4 '>
 
-                <DateSelect currentDate={currentDate} onChange={setDate} />
-
-
-                <div className='space-y-4 '>
-                    {isLoading && 'Carregando jogos...'}
-                    {hasError && 'Ops! Algo deu errado! '}
-
-
-
-                    {isDone && games.value?.map(game => (
-                        <Card
-                            key={game.id}
-                            gameId={game.id}
-                            homeTeam={game.homeTeam}
-                            awayTeam={game.awayTeam}
-                            gameTime={format(new Date(game.gameTime), 'H:mm')}
-                            homeTeamScore={user?.hunches?.[game.id]?.homeTeamScore || ''}
-                            awayTeamScore={user?.hunches?.[game.id]?.awayTeamScore || ''}
-
-                        />
-                    ))}
-                </div>
+                    <DateSelect currentDate={currentDate} onChange={setDate} />
 
 
-            </section>
-        </main>
-    </Fragment>
-)
+                    <div className='space-y-4 '>
+                        {isLoading && 'Carregando jogos...'}
+                        {hasError && 'Ops! Algo deu errado! '}
+
+
+
+                        {isDone && games.value?.map(game => (
+                            <Card
+                                key={game.id}
+                                gameId={game.id}
+                                homeTeam={game.homeTeam}
+                                awayTeam={game.awayTeam}
+                                gameTime={format(new Date(game.gameTime), 'H:mm')}
+                                homeTeamScore={user?.hunches?.[game.id]?.homeTeamScore || ''}
+                                awayTeamScore={user?.hunches?.[game.id]?.awayTeamScore || ''}
+
+                            />
+                        ))}
+                    </div>
+
+
+                </section>
+            </main>
+        </Fragment>
+    )
 }
