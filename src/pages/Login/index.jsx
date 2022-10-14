8381fae800cc26dbbe0652460bf5e88a8ba0612b
hlruffo <hlruffo@gmail.com>
2022-10-14 12:43:12 -0300

    feat removed heroku everywhere

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -28,7 +28,7 @@ export const Login = () => {
 
             const res = await axios({
                 method: 'get',
-                baseURL: `https://cors-anywhere.herokuapp.com/${import.meta.env.VITE_API_URL}`,
+                baseURL: import.meta.env.VITE_API_URL,
                 url: '/login',
                 auth: {
                     username: values.email,
